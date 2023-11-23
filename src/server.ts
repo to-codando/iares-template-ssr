29c4b414849cda7912bece9ae37cb4aec65e56a2
Rodrigo Rocha <rocha.webdev@gmail.com>
2023-11-23 02:48:11 -0300

    update config

--- src/server.ts
+++ src/server.ts
@@ -1,32 +1,29 @@
-import { Router } from '@stricjs/router';
-import * as send from '@stricjs/app/send';
 import { AppHome } from '@/components/AppHome';
 import globalJsdom from 'global-jsdom';
-import { render, html } from 'iares';
 import { Elysia } from "elysia";
 import { staticPlugin } from '@elysiajs/static'
-import { html as HTMLElysia} from '@elysiajs/html'
-
+import { html as HTMLElysia } from '@elysiajs/html'
 import PageLayout from './Layouts/Page';
-let cleanDom = null;
 
-    
+let cleanDom = null;
 const app = new Elysia()
 
-app.use(staticPlugin({assets: 'dist/assets', prefix: '/assets'}))
+app.use(staticPlugin({ assets: 'dist/assets', prefix: '/assets' }))
 app.use(HTMLElysia())
 
-  app.get('/', () => { 
-    cleanDom = globalJsdom();
-    const HomePage = PageLayout(AppHome, {title: 'Outro'})
-    cleanDom()  
-    return HomePage;
-  });
-  app.get('/main.js', () => { 
-    return Bun.file('dist/main.js');
-  });
-  app.get('/main.css', () => { 
-    return Bun.file('dist/assets/styles/main.css');
-  });
+app.get('/', () => {
+  cleanDom = globalJsdom();
+  const HomePage = PageLayout(AppHome, { title: 'Outro' })
+  cleanDom()
+  return HomePage;
+});
 
-  app.listen(4000);
+app.get('/main.js', () => {
+  return Bun.file('dist/main.js');
+});
+
+app.get('/main.css', () => {
+  return Bun.file('dist/assets/styles/main.css');
+});
+
+app.listen(4000);
